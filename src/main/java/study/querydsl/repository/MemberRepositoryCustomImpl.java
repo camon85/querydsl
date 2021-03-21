daadabe8bfe81ae3b8d05369e7743364e980e6a8
camon <camon85@gmail.com>
2021-03-21 20:09:24 +0900

    spring data 페이징 활용 - 불필요한 count query 실행되지 않도록 최적화

--- src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
@@ -2,14 +2,17 @@ package study.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
 
 import java.util.List;
 
@@ -91,7 +94,7 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
         .fetch();
 
     // count 쿼리를 분리하여 성능 최적화 가능한 경우 쿼리를 분리할 수도 있다.
-    long total = queryFactory
+    JPAQuery<Member> countQuery = queryFactory
         .select(member)
         .from(member)
         .leftJoin(member.team, team)
@@ -100,10 +103,14 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
             teamNameEq(condition.getTeamName()),
             ageGoe(condition.getAgeGoe()),
             ageLoe(condition.getAgeLoe())
-        )
-        .fetchCount();
+        );
 
-    return new PageImpl<>(content, pageable, total);
+    /*
+    count 쿼리가 생략 가능한 경우 생략해서 처리
+    1. 페이지 시작이면서 컨텐츠 사이즈가 페이지 사이즈보다 작을 때
+    2. 마지막 페이지 일 때 (offset + 컨텐츠 사이즈를 더해서 전체 사이즈 구함
+     */
+    return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
   }
 
   private BooleanExpression usernameEq(String username) {
